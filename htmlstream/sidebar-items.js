58745af2ac27956f8c1f9c19def4bc91ce7b31c4
Zongmin Lei <leizongmin@gmail.com>
2015-08-16 21:40:01 +0800

    doc

---
+++ htmlstream/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["HTMLTagState","The tag state"]],"fn":[["parse_attributes","Parse attributes strings, find the attribute and call `on_tag_attribute()` every time"],["parse_html","Parse HTML source, find the tags and call `on_tag()` every time"]],"struct":[["HTMLTag","The HTML tag"],["HTMLTagAttribute","The tag attribute"],["Position","The HTML source position"]]});
