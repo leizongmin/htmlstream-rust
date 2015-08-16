75f3ca20b9313b2449536499312d5238798b0721
Zongmin Lei <leizongmin@gmail.com>
2015-08-16 20:20:06 +0800

    parse_attributes()

--- src/lib.rs
+++ src/lib.rs
@@ -156,6 +156,7 @@ pub fn parse_html<F>(html: &str, on_tag: F) where F: Fn(&Position, &HTMLTag) {
                 is_tag_start = true;
                 is_get_tag_name = false;
                 is_closing_tag = false;
+                is_quote_start = false;
                 last_index = current_index - 1;
                 continue;
             }
@@ -165,7 +166,7 @@ pub fn parse_html<F>(html: &str, on_tag: F) where F: Fn(&Position, &HTMLTag) {
 
     // 剩余部分的文本
     if last_index < current_index - 1 {
-        let tag_html = &html[last_index..(current_index - 1)];
+        let tag_html = &html[last_index..];
         let position = Position { start: last_index, end: current_index };
         let tag = HTMLTag {
             name: "".to_string(),
@@ -177,15 +178,148 @@ pub fn parse_html<F>(html: &str, on_tag: F) where F: Fn(&Position, &HTMLTag) {
     }
 }
 
-pub fn parse_attribute<F>(html: &String, on_tag_attribute: F) where F: Fn(&Position, &HTMLTagAttribute) {
+pub fn parse_attributes<F>(html: &String, on_tag_attribute: F) where F: Fn(&Position, &HTMLTagAttribute) {
+    let mut is_quote_start: bool = false;
+    let mut is_attribute_start: bool = false;
+    let mut is_get_attribute_name: bool = false;
 
+    let mut quote_char: u8 = 0;
+    let mut last_char: u8 = 0;
+    let mut last_index: usize = 0;
+    let mut current_index: usize = 0;
+    let mut value_start_index: usize = 0;
+
+    let html_bytes = html.as_bytes();
+    for b in html_bytes {
+        let c = *b;
+        if current_index > 0 {
+            last_char = html_bytes[current_index - 1];;
+        }
+        current_index += 1;
+
+        if is_attribute_start {
+
+            if is_get_attribute_name {
+
+                // 如果当前字符出现在引号内，只有遇到相同的引号才能结束
+                if is_quote_start {
+                    if c == quote_char {
+                        // 如果当前字符出现在引号内，只有遇到相同的引号才能结束
+                        if c == quote_char {
+                            let name = &html[last_index..(value_start_index - 1)];
+                            let value = &html[(value_start_index + 1)..(current_index - 1)];
+                            let position = Position { start: last_index, end: current_index };
+                            let attribute = HTMLTagAttribute {
+                                name: name.to_string(),
+                                value: value.to_string(),
+                            };
+                            on_tag_attribute(&position, &attribute);
+
+                            is_attribute_start = false;
+                            is_quote_start = false;
+                            is_get_attribute_name = false;
+                            continue;
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+
+                // 引号开始
+                if CHAR_SINGLE_QUOTE == c || CHAR_DOUBLE_QUOTE == c {
+                    // 仅当前一个字符为等于号时引号才有作用
+                    if CHAR_EQUAL == last_char {
+                        is_quote_start = true;
+                        quote_char = c;
+                    }
+                    continue;
+                }
+
+                // 空格结束当前标签
+                if c <= CHAR_SPACE {
+                    let name = &html[last_index..(value_start_index - 1)];
+                    let value = &html[(value_start_index)..(current_index - 1)];
+                    let position = Position { start: last_index, end: current_index };
+                    let attribute = HTMLTagAttribute {
+                        name: name.to_string(),
+                        value: value.to_string(),
+                    };
+                    on_tag_attribute(&position, &attribute);
+
+                    is_attribute_start = false;
+                    is_quote_start = false;
+                    is_get_attribute_name = false;
+                    continue;
+                }
+
+            } else {
+
+                // 等于号表示属性值开始
+                if CHAR_EQUAL == c {
+                    value_start_index = current_index;
+                    is_get_attribute_name = true;
+                    continue;
+                }
+
+                // 空格结束当前标签
+                if c <= CHAR_SPACE {
+                    let name = &html[last_index..(current_index - 1)];
+                    let position = Position { start: last_index, end: current_index };
+                    let attribute = HTMLTagAttribute {
+                        name: name.to_string(),
+                        value: "".to_string(),
+                    };
+                    on_tag_attribute(&position, &attribute);
+
+                    is_attribute_start = false;
+                    is_quote_start = false;
+                    is_get_attribute_name = false;
+                    continue;
+                }
+
+            }
+
+        } else {
+
+            // 忽略空白字符
+            if c <= CHAR_SPACE {
+                continue;
+            }
+
+            is_attribute_start = true;
+            is_get_attribute_name = false;
+            is_quote_start = false;
+            last_index = current_index - 1;
+
+        }
+    }
+
+    // 剩余部分的文本
+    let name = &html[last_index..];
+    let position = Position { start: last_index, end: current_index };
+    let attribute = HTMLTagAttribute {
+        name: name.to_string(),
+        value: "".to_string()
+    };
+    on_tag_attribute(&position, &attribute);
 }
 
 
-#[test]
-fn test_parse_html() {
-    parse_html("<b>这里是html: <a href='#' title=ok>link</a> yes <b /> goosa", |pos: &Position, tag: &HTMLTag| {
+fn test(html: &str) {
+    println!("\n");
+    parse_html(html, |pos: &Position, tag: &HTMLTag| {
         println!("{:?} {:?}", pos, tag);
-        //println!("{:?}", tag.html);
+        if tag.attributes.len() > 0 {
+            println!("  parse: {:?}", &tag.attributes);
+            parse_attributes(&tag.attributes, |pos: &Position, attr: &HTMLTagAttribute| {
+                println!("  -> {:?} {:?}", pos, attr);
+            });
+        }
     });
+    println!("\n");
+}
+
+fn main() {
+    //test("<b>这里是html: <a href='#' title=ok disabled>link</a> yes <b /> goosa");
+    test("<a \"ok\" href=\"<a href=\"#\"target=_blank title=\"haha\" disabled>A</a>\">B</a>");
 }
